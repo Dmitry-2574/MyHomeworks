88e5ecd1233362da4ba9b1bf7d40910a5f023e1b
Dim <d@mal>
2024-12-15 22:38:15 +0700

    написал тест csv

--- Doom_24/files_utils_test.py
+++ Doom_24/files_utils_test.py
@@ -11,3 +11,13 @@ append_json([{"name": "Jane Doe", "age": 25, "city": "London"}], "test.json")
 read_data_json_append = read_json("test.json")
 print(f"JSON appended: {read_data_json_append}")
 
+# CSV
+data_csv = [["Name", "Age", "City"], ["John Doe", "30", "New York"]]
+write_csv(data_csv, "test.csv")
+read_data_csv = read_csv("test.csv")
+print(f"CSV read: {read_data_csv}")
+
+append_csv([["Jane Doe", "25", "London"]], "test.csv")
+read_data_csv_append = read_csv("test.csv")
+print(f"CSV appended: {read_data_csv_append}")
+
