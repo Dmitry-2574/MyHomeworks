4cbe129d641adc8d0115ca5d8677a72d87787dcc
Dim <d@mal>
2024-12-23 18:50:39 +0700

    сделаны п. 2 и 3 задания 25

--- Doom_25/Doom_25.py
+++ Doom_25/Doom_25.py
@@ -1,4 +1,13 @@
 from marvel import full_dict
 from pprint import pprint
 
-pprint(full_dict)
+# 2. Реализуйте ввод от пользователя, который будет принимать цифры через пробел. Разбейте введённую строку на список и примените к каждому элементу `int`, заменяя нечисловые элементы на `None` с помощью `map`.
+number_input = input("Введите числа через пробел: ")
+numbers = list(map(lambda x: int(x) if x.isdigit() else None, number_input.split()))
+print(f"Обработанный список: {numbers}")
+
+# 3. Используйте `filter`, чтобы создать словарь, содержащий исходные `id` и другие ключи, но только для тех фильмов, `id` которых присутствуют в списке, полученном на предыдущем шаге.
+filtered_movies = dict(filter(lambda item: item[0] in numbers and item[0] is not None, full_dict.items()))
+pprint(filtered_movies)
+
+
