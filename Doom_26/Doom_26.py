4f3be5ada83b16170cc5d27c24821a132e454d18
Dim <d@mal>
2025-01-02 13:31:04 +0700

    Домашка 26 сделано проверка кода и вывод ошибки

--- Doom_26/Doom_26.py
+++ Doom_26/Doom_26.py
@@ -4,19 +4,27 @@ def password_checker(func):
     # Это декоратор , В данном случае func — это функция register_user которую мы будем украшать
     def wrapper(password):
         #Эта функция будет выполнять фактическую проверку пароля.  Она принимает пароль password в качестве аргумента. Внутри данной функции идет проверка пароля.
-        if len(password)>8:
+        if len(password)<8:
             return "Ошибка: Пароль должен содержать минимум 8 символов. "
             #re.search() ищет соответствие регулярному выражению внутри строки.  Если хотя бы одно условие не выполняется, функция wrapper возвращает сообщение об ошибке.
-        if not re.search("0-9", password):
+        if not re.search("[0-9]", password):
             return "Ошибка: Пароль должен содержать хотя бы одну цифру."
         if not re.search("[A-Z]", password):
             return "Ошибка: Пароль должен содержать хотя бы одну заглавную букву."
         if not re.search("[a-z]", password):
             return "Ошибка: Пароль должен содержать хотя бы одну строчную букву."
-        if not re.search("[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]", password):
+        if not re.search("[^a-zA-Z0-9]", password):
             return "Ошибка: Пароль должен содержать хотя бы один специальный символ."
         return func(password) 
         # Если все условия выполнены, выполняем функцию register_user с этим паролем.
     return wrapper
 
+@password_checker
+def register_user(password):
+    return "Регистрация успешна"
+
+print(register_user("P@sswOrd1")) # Все условия соблюдены
+print(register_user("Short"))  # Слишком короткий
+print(register_user("12345678")) # Нет заглавных/строчных/спецсимволов
+print(register_user("Password123")) # Нет спецсимволов
 
